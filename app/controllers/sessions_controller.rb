b0c872a2744fedaf4e7a52bd2576d6b12ea20f3f
Kevin <kevinwang2400@gmail.com>
2015-06-11 10:59:04 -0700

    errors styling on user auth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -14,6 +14,7 @@ class SessionsController < ApplicationController
       log_in(user)
       redirect_to "/#home"
     else
+      @user = User.new
       flash.now[:errors] = ["Invalid username or password"]
       render :new
     end
