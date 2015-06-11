44300a3d8389f0452c67d86951b0f08796858607
Kevin <kevinwang2400@gmail.com>
2015-06-11 16:32:56 -0700

    refactored project detail editing without subviews

--- app/assets/javascripts/views/projects/project_show.js
+++ app/assets/javascripts/views/projects/project_show.js
@@ -7,7 +7,8 @@ BasecampApp.Views.ProjectShow = Backbone.CompositeView.extend({
     'click .project-title': "editTitle",
     'click .project-description-text': 'editDescription',
     'click .upload': "upload",
-    'blur input': 'uptdateAndSave'
+    'blur input': 'updateAndSave',
+    'blur textarea': 'updateAndSave'
   },
 
   initialize: function () {
@@ -17,24 +18,28 @@ BasecampApp.Views.ProjectShow = Backbone.CompositeView.extend({
 
   editTitle: function (event) {
     event.preventDefault();
-    var form = new BasecampApp.Views.Editable({ 
-      model: this.model, 
-      field: "title",
-      fieldType: "text"
-    });
-    this.$el.find('.project-title').html(form.render().$el);
-    $(event.currentTarget).removeClass('project-title');
+    var $input = $('<input>')
+      .addClass('project-title-editing')
+      .data("attr", 'title')
+      .data("input", "input")
+      .val($(event.currentTarget).html());
+
+    this.$el.find('.project-title').html($input);
+    $input.focus();
+    this.$el.find('.project-title').removeClass('project-title')
   },
 
   editDescription: function (event) {
     event.preventDefault();
-    var form = new BasecampApp.Views.Editable({ 
-      model: this.model, 
-      field: "description",
-      fieldType: "textarea"
-    });
-    this.$el.find('.project-description-text').html(form.render().$el);
-    $(event.currentTarget).removeClass('project-description-text');
+    var $input = $('<textarea>')
+      .addClass('project-description-text-editing')
+      .data("attr", 'description')
+      .data("input", "textarea")
+      .val($(event.currentTarget).html());
+
+    this.$el.find('.project-description-text').html($input);
+    $input.focus();
+    this.$el.find('.project-description-text').removeClass('project-description-text')
   },
 
   inviteUsers: function (event) {
@@ -46,6 +51,14 @@ BasecampApp.Views.ProjectShow = Backbone.CompositeView.extend({
     var content = this.template({ project: this.model });
     this.$el.html(content);
     return this;
+  },
+
+  updateAndSave: function (event) {
+    var attr = $(event.currentTarget).data('attr');
+    var input = $(event.currentTarget).data('input');
+    var newAttr = {};
+    newAttr[attr] = this.$el.find(input).val();
+    this.model.save(newAttr);
   },
 
   upload: function (event) {
