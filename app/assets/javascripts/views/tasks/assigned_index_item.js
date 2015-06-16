91898a408f8c31006469e62947e14a4cc342e15c
Kevin <kevinwang2400@gmail.com>
2015-06-16 16:51:39 -0700

    styling task index item

--- app/assets/javascripts/views/tasks/assigned_index_item.js
+++ app/assets/javascripts/views/tasks/assigned_index_item.js
@@ -1,13 +1,41 @@
 BasecampApp.Views.AssignedIndexItem = Backbone.View.extend({
   template: JST['tasks/assigned_index_item'],
+  className: "member-circle-img",
+
+  attributes: function () {
+    return { 'data-id': this.model.get('user-_id' )}
+  },
 
   initialize: function () {
     this.listenTo(this.model, 'sync', this.render);
+    this.listenTo(this.model, 'destroy', this.remove);
   },
 
   render: function () {
     var content = this.template({ user: this.model });
     this.$el.html(content);
+
+    // hacking profile pictures for now -__-
+    switch(this.model.get('user_id')) {
+      case 1:
+        this.$el.find('.member').attr('id', 'c1');
+        break;
+      case 2:
+        this.$el.find('.member').attr('id', 'c2');
+        break;
+      case 3:
+        this.$el.find('.member').attr('id', 'c3');
+        break;
+      default:
+        this.$el.find('.member').attr('id', 'c-default');
+        break;
+    }
+    //
+
+    setTimeout(function () {
+      this.$el.draggable({ revert: true });
+    }.bind(this), 0)
+
     return this;
   }
 });
