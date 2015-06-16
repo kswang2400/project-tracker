f82f7a604ae4924177f7a4330d3a930743a2fed8
Kevin <kevinwang2400@gmail.com>
2015-06-16 16:33:32 -0700

    write parse and getter functions for project model

--- app/assets/javascripts/models/project.js
+++ app/assets/javascripts/models/project.js
@@ -1,3 +1,33 @@
 BasecampApp.Models.Project = Backbone.Model.extend({
-  urlRoot: "/api/projects"
+  urlRoot: "/api/projects",
+
+  memberships: function () {
+    this._memberships = this._memberships || new BasecampApp.Collections.Memberships();
+    return this._memberships;
+  },
+
+  parse: function (payload) {
+    if (payload.uploads) {
+      this.uploads().set(payload.uploads);
+      delete payload.uploads;
+    } elsif (payload.tasks) {
+      this.tasks().set(payload.tasks, { parse: true });
+      delete payload.tasks;
+    } elsif (payload.memberships) {
+      this.memberships().set(payload.memberships);
+      delete payload.memberships
+    }
+
+    return payload;
+  },
+
+  tasks: function () {
+    this._tasks = this._tasks || new BasecampApp.Collections.Tasks();
+    return this._tasks;
+  },
+  
+  uploads: function () {
+    this._uploads = this._uploads || new BasecampApp.Collections.Uploads();
+    return this._uploads;
+  }
 });
