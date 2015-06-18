12c557085c11a10ad0b1c9eb4ac8612b5da35acd
Kevin <kevinwang2400@gmail.com>
2015-06-18 08:12:02 -0700

    refactor seed functions

--- app/controllers/api/projects_controller.rb
+++ app/controllers/api/projects_controller.rb
@@ -5,51 +5,7 @@ module Api
       project.owner_id = current_user.id
 
       if project.save
-        Membership.create!(user_id: 1, project_id: project.id)
-        Membership.create!(user_id: 2, project_id: project.id)
-        Membership.create!(user_id: 3, project_id: project.id)
-
-        task1 = Task.create!(
-          author_id: 1,
-          project_id: project.id,
-          title: "First Task!",
-          body: "Hello! We're here to get you started! Drag anyone to any task to create an assignment!"
-        )
-
-        task2 = Task.create!(
-          author_id: 1,
-          project_id: project.id,
-          title: "Second Task!",
-          body: "If they're not doing a good job, drag them to the X to remove them from task"
-        )
-
-        task3 = Task.create!(
-          author_id: 1,
-          project_id: project.id,
-          title: "Last Task!",
-          body: "If they REALLY suck, kick them out of the project. Drag them from the bar to the X to remove them from the project"
-        )
-
-        AssignedTask.create!(
-          user_id: 1,
-          task_id: task1.id
-        )
-
-        AssignedTask.create!(
-          user_id: 3,
-          task_id: task1.id
-        )
-
-        AssignedTask.create!(
-          user_id: 2,
-          task_id: task2.id
-        )
-
-        AssignedTask.create!(
-          user_id: 1,
-          task_id: task3.id
-        )
-
+        seed_new_project(project)
         render json: project
       else
         render json: project.errors.fullmessages, status: :unprocessable_entity
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -31,4 +31,60 @@ class ApplicationController < ActionController::Base
   def require_log_in!
     redirect_to new_session_url unless logged_in?
   end
+
+  def seed_new_user(user)
+    project = Project.create!(
+      owner_id: user.id, 
+      title: "Demo Project!", 
+      description: "Click here for more info."
+    )
+    seed_new_project(project)
+  end
+
+  def seed_new_project(project)
+    Membership.create!(user_id: 1, project_id: project.id)
+    Membership.create!(user_id: 2, project_id: project.id)
+    Membership.create!(user_id: 3, project_id: project.id)
+
+    task1 = Task.create!(
+      author_id: 1,
+      project_id: project.id,
+      title: "First Task!",
+      body: "Hello! We're here to get you started! Drag anyone to any task to create an assignment!"
+    )
+
+    task2 = Task.create!(
+      author_id: 1,
+      project_id: project.id,
+      title: "Second Task!",
+      body: "If they're not doing a good job, drag them to the X to remove them from task"
+    )
+
+    task3 = Task.create!(
+      author_id: 1,
+      project_id: project.id,
+      title: "Last Task!",
+      body: "If they REALLY suck, kick them out of the project. Drag them from the bar to the X to remove them from the project"
+    )
+
+    AssignedTask.create!(
+      user_id: 1,
+      task_id: task1.id
+    )
+
+    AssignedTask.create!(
+      user_id: 3,
+      task_id: task1.id
+    )
+
+    AssignedTask.create!(
+      user_id: 2,
+      task_id: task2.id
+    )
+
+    AssignedTask.create!(
+      user_id: 1,
+      task_id: task3.id
+    )
+  end
 end
