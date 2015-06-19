4b665194cc24a28582738b5609c5e577918fd89d
Kevin <kevinwang2400@gmail.com>
2015-06-19 10:43:27 -0700

    form styling

--- app/assets/javascripts/views/users/result.js
+++ app/assets/javascripts/views/users/result.js
@@ -1,5 +1,6 @@
 BasecampApp.Views.Result = Backbone.View.extend({
   template: JST['users/result'],
+  className: "search-results",
 
   render: function () {
     var content = this.template({ user: this.model });
