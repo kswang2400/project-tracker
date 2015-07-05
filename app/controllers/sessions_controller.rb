54cc89584c1fae62ddc101a8a6808b769256bff2
Kevin <kevinwang2400@gmail.com>
2015-07-04 23:10:27 -0700

    remove page scroll from session form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -23,6 +23,6 @@ class SessionsController < ApplicationController
   def destroy
     log_out!
     # redirect_to new_session_url
-    render json: {} # FIX THIS
+    render body: "hehe, you caught me xP"  # FIX THIS
   end
 end
