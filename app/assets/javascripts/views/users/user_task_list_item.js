64d24e4581d399a7480ff85e8ff3cecdf31c518f
Kevin <kevinwang2400@gmail.com>
2015-07-06 12:57:47 -0700

    task complete/delete in user show

--- app/assets/javascripts/views/users/user_task_list_item.js
+++ app/assets/javascripts/views/users/user_task_list_item.js
@@ -2,6 +2,11 @@ BasecampApp.Views.UserTaskListItem = Backbone.View.extend({
   template: JST['tasks/user_task_list_item'],
   className: "task-list-item clearfix",
 
+  events: {
+    'click .complete-task': "completeTask",
+    'click .delete-task': "deleteTask",
+  },
+
   intialize: function () {
     this.listenTo(this.model, 'sync', this.render);
   },
@@ -14,6 +19,18 @@ BasecampApp.Views.UserTaskListItem = Backbone.View.extend({
     } else {
       taskStatus.addClass("complete");
     }
+  },
+
+  completeTask: function (event) {
+    if (this.model.get('status') !== "completed") {
+      this.model.save({ status: "completed" }, { patch: true });
+    }
+    // move View to completed section?
+  },
+
+  deleteTask: function () {
+    this.model.destroy();
+    this.remove();
   },
 
   render: function () {
