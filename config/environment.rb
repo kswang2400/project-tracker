e932d3db94fc688404fe08d4e450a78cff97f625
Kevin <kevinwang2400@gmail.com>
2015-08-05 10:46:28 -0700

    remove pending test

--- config/environment.rb
+++ config/environment.rb
@@ -3,18 +3,3 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
-config.action_mailer.default_url_options = { host: 'twinkieset.com' }
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.delivery_method = :smtp
-  host = 'twinkieset.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
