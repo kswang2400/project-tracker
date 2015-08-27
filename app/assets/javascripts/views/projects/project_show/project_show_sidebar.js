35e9d897324a0c48c9c30e0813ea68bd017bcf08
Kevin <kevinwang2400@gmail.com>
2015-08-27 01:42:50 -0700

    move logic, much cleaner

--- app/assets/javascripts/views/projects/project_show/project_show_sidebar.js
+++ app/assets/javascripts/views/projects/project_show/project_show_sidebar.js
@@ -1,6 +1,16 @@
 BasecampApp.Views.ProjectShowSidebar = Backbone.CompositeView.extend({
   template: JST["projects/project_show/sidebar"],
 
+  events: {
+    "click .invite-users": "inviteUsers",
+    "click .project-title": "editTitle",
+    "click .project-description-text": "editDescription",
+    "click .tag-user": "tagUser",
+    "click .upload-button": "upload",
+    "blur input.editing": "updateAndSave",
+    "blur textarea": "updateAndSave"
+  },
+
   initialize: function () {
     this.listenTo(this.model, "change", this.render);
 
@@ -32,11 +42,98 @@ BasecampApp.Views.ProjectShowSidebar = Backbone.CompositeView.extend({
     });
     this.addSubview(".project-user-search", subview);
   },
+
+  editTitle: function (event) {
+    event.preventDefault();
+    var $input = $("<input>")
+      .addClass("project-title editing")
+      .data("attr", "title")
+      .data("input", "input")
+      .val($(event.currentTarget).html());
+
+    this.$el.find(".project-title").html($input);
+    $input.focus();
+    this.$el.find(".project-title").removeClass("project-title");
+  },
+
+  editDescription: function (event) {
+    event.preventDefault();
+    var $input = $("<textarea>")
+      .addClass("project-description-text editing")
+      .data("attr", "description")
+      .data("input", "textarea")
+      .val($(event.currentTarget).html());
+
+    this.$el.find(".project-description-text").html($input);
+    $input.focus();
+    this.$el.find(".project-description-text")
+      .addClass("project-description-text-editing")
+      .removeClass("project-description-text");
+  },
+
+  inviteUsers: function (event) {
+    event.preventDefault();
+    var searchBar = this.$el.find(".project-user-search");
+    searchBar.toggleClass("hidden");
+    $("body").find(".users-search").usersSearch();
+
+    if (searchBar.hasClass("hidden")) {
+      this.$el.find(".project-show-upload-bar").removeClass("hidden");
+    } else {
+      this.$el.find(".project-show-upload-bar").addClass("hidden");
+    }
+  },
   
   render: function () {
     var content = this.template({ project: this.model });
     this.$el.html(content);
     this.attachSubviews();
     return this;
+  },
+
+  tagUser: function (event) {
+    var userId = $(event.currentTarget).attr("id");
+    var attr = { user_id: userId };
+
+    var membership = new BasecampApp.Models.Membership({
+      project_id: this.model.id
+    });
+
+    membership.save(attr, {
+      success: function () {
+        this.model.memberships().add(membership, { merge: true });
+        this.$el.find(".project-user-search").toggleClass("hidden");
+        this.$el.find(".project-show-upload-bar").removeClass("hidden");
+      }.bind(this)
+    });
+  },
+
+  updateAndSave: function (event) {
+    var attr = $(event.currentTarget).data("attr");
+    var input = $(event.currentTarget).data("input");
+    var newAttr = {};
+    newAttr[attr] = $("#project-show-main").find(input).val();
+    this.model.save(newAttr);
+
+    if (attr === "title") {
+      $("h1#project-title").remove("input").addClass("project-title").text(newAttr[attr]);
+    } else {
+      $("p#description").remove("input").addClass("project-description-text").text(newAttr[attr]);
+    }
+  },
+
+  upload: function (event) {
+    event.preventDefault();
+    cloudinary.openUploadWidget(window.CLOUDINARY_SETTINGS, function(error, payload) {
+      var attrs = {
+        url: payload[0].url,
+        thumbnail_url: payload[0].thumbnail_url
+      };
+      new BasecampApp.Models.Upload({ project: this.model }).save(attrs, {
+        success: function () {
+          this.model.uploads().fetch();
+        }.bind(this)
+      });
+    }.bind(this));
   }
 })
